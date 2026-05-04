0cdc912df4cce45f50411c84d3386a185ef6f1c7
deliafarcas <125966792+deliafarcas@users.noreply.github.com>
2026-05-04 11:31:40 +0300

    modifications for the bronze challenge

--- myfarm/src/app/app.config.ts
+++ myfarm/src/app/app.config.ts
@@ -3,7 +3,6 @@ import { provideRouter } from '@angular/router';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { CookieService } from 'ngx-cookie-service';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { offlineInterceptor } from './services/offline.interceptor';
 
 import { routes } from './app.routes';
 
@@ -12,7 +11,6 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideCharts(withDefaultRegisterables()),
-    provideHttpClient(withInterceptors([offlineInterceptor])),
     CookieService,
   ],
 };
