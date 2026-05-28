eedccd73d1a11776d284146858cf0373abbcec25
deliafarcas <125966792+deliafarcas@users.noreply.github.com>
2026-05-28 21:42:03 +0300

    the last one i promise

--- myfarm-backend/src/main/java/org/example/myfarmbackend/config/SecurityConfig.java
+++ myfarm-backend/src/main/java/org/example/myfarmbackend/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("http://localhost:4200"));
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:4200", "https://ferma-mea.up.railway.app/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With", "Accept", "Origin"));
         configuration.setAllowCredentials(true);
--- myfarm/src/environments/environment.ts
+++ myfarm/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://angular-farm-application-production.up.railway.app/' 
+  apiUrl: 'https://angular-farm-application-production.up.railway.app' 
 };
